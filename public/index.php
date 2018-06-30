8ffb5599e53cc9ccf93cd55fb6fbb70d1aa95429
alimatin <alimatin221@yahoo.com>
2018-06-30 19:39:34 +0430

    Register autoloaders

--- public/index.php
+++ public/index.php
@@ -12,4 +12,17 @@
  * @copyright Copyright (c) 2017-2018, ILYA-IDEA Company
  */
 
-echo "<b>Hello</b> public/index.php";
+use Phalcon\Loader;
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__). '/');
+define('APP_PATH', BASE_PATH. 'app/');
+
+// Register AutoLoaders
+$loader = new Loader();
+$loader->registerDirs(
+    [
+        APP_PATH. 'controllers/',
+        APP_PATH. 'models/'
+    ]
+)->register();
