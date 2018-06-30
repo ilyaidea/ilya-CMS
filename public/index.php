96befb06563bff5fb3b3cd1b5b9cfe4fcd2c5332
alimatin <alimatin221@yahoo.com>
2018-06-30 20:07:32 +0430

    Config Services And register application for send

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@
  */
 
 use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
 
 // Define some absolute path constants to aid in locating resources
 define('BASE_PATH', dirname(__DIR__). '/');
@@ -23,6 +24,34 @@ $loader = new Loader();
 $loader->registerDirs(
     [
         APP_PATH. 'controllers/',
-        APP_PATH. 'models/'
+        APP_PATH. 'models/',
+        BASE_PATH. 'lib/'
     ]
 )->register();
+
+// Create a di
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set('view', function () {
+    $view = new \Phalcon\Mvc\View();
+    $view->setViewsDir(APP_PATH. 'views');
+    return $view;
+});
+
+$di->set('url', function () {
+    $url = new \Phalcon\Mvc\Url();
+    $url->setBaseUri('/cms/');
+    return $url;
+});
+
+$application = new \Phalcon\Mvc\Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
