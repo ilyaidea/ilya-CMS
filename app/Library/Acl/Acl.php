5e2bbdf5a4b25829ed060f2e94f829fdf05f0dc4
IlyaIdea <alimansoori71@outlook.com>
2018-09-08 17:37:54 +0430

    init acl

---
+++ app/Library/Acl/Acl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Summary File Acl
+ *
+ * Description File Acl
+ *
+ * ILYA CMS Created by ILYA-IDEA Company.
+ * @author Ali Mansoori
+ * Date: 8/13/2018
+ * Time: 6:50 PM
+ * @version 1.0.0
+ * @copyright Copyright (c) 2017-2018, ILYA-IDEA Company
+ */
+namespace Lib\Acl;
+
+use Phalcon\Mvc\User\Plugin;
+
+class Acl extends Plugin
+{
+
+}
