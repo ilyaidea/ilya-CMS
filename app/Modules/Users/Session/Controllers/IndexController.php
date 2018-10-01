fc22f62476bc2ce319a8573df10e1f305346a87c
IlyaIdea <alimansoori71@outlook.com>
2018-10-01 17:18:32 +0330

    commit test

--- app/Modules/Users/Session/Controllers/IndexController.php
+++ app/Modules/Users/Session/Controllers/IndexController.php
@@ -13,9 +13,10 @@
  */
 namespace Modules\Users\Session\Controllers;
 
-use Ilya\Models\Lang;
-use Modules\Users\Session\Forms\LoginForm;
-use Modules\Users\Session\Models\UserFieldsCategory;
+use Ilya\Models\Users;
+use Lib\Mvc\Test;
+use Modules\Users\Session\Lib\Test\Person;
+use Modules\Users\Session\Lib\Test\PhoneNumber;
 use Phalcon\Mvc\View;
 
 class IndexController extends \Lib\Mvc\Controller
@@ -23,31 +24,43 @@ class IndexController extends \Lib\Mvc\Controller
     public function indexAction()
     {
         echo "<pre>";
+//        $session = $this->session->get('auth');
+//
+//        $id = $session->id;
+//
+//        $user = Users::findFirstById($id);
+//
+//
+//        die(
+//            print_r($user->getRole())
+//        );
 
-//        $categories = UserFieldsCategory::findFirst(1);
+        // Client
 
-        $langs = Lang::find();
+        $phone = new PhoneNumber();
 
-        $row = [];
-        foreach ($langs as $lang)
-        {
-            $testArray = [];
-            $testArray['title'] = $lang->title;
-            $testArray['value'] = $lang->value;
+        $person = new Person($phone);
 
-            $testArray['categories'] = [];
-            foreach ($lang->userFieldsCategories as $key => $category)
-            {
-                $testArray['categories'][$key]['title'] = $category->title;
-                $testArray['categories'][$key]['fields'] = $category->getFields()->toArray();
-            }
+        $person->setName('reza');
 
-            $row[] = $testArray;
-        }
+        $person->getHomePhone()->setAreaCode('025');
+        $person->getHomePhone()->setPhoneNumber('32916547');
 
-        print_r($row);
+        $person->getOfficePhone()->setAreaCode('021');
+        $person->getOfficePhone()->setPhoneNumber('2315489');
 
+        echo "Home => ". $person->getHomePhone()->getNumber();
+
+        echo "<br>";
+        echo "<br>";
+
+        echo "Office => ". $person->getOfficePhone()->getNumber();
 
         $this->view->disable();
+    }
+
+    public function errorAction()
+    {
+        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
     }
 }
