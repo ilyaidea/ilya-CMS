d7e658169f62a9a3536316df4285a68ec8774725
Unknown <saeeddavari72@gmail.com>
2018-12-26 11:37:33 +0330

    Model DbRef
    add a new properties DbRef 
    we can switch between different databases 
    by change a value DbRef true it will read from base dbname 
    other way will find from module

--- app/Library/Mvc/Model.php
+++ app/Library/Mvc/Model.php
@@ -27,24 +27,46 @@ class Model extends \Phalcon\Mvc\Model
     protected $helper;
     protected $update_mode = false;
     protected $create_mode = false;
+    protected $dbRef = false;
 
     public function initialize()
     {
-
         if(method_exists($this,'init'))
             $this->init();
+
+        if($this->getDI()->getShared('config')->database->dbname)
+            $this->setSchema($this->getDI()->getShared('config')->database->dbname);
 
         if(method_exists($this,'relations'))
             $this->relations();
 
         $this->helper = $this->getDI()->getShared('helper');
 
-        if(isset($this->getDI()->getShared('config')->module->database->connection))
+        if(isset($this->getDI()->getShared('config')->module->database->connection) && !$this->isDbRef() )
         {
             $this->setConnectionService($this->getDI()->getShared('config')->module->database->connection);
+            $this->setSchema($this->getDI()->getShared('config')->module->database->dbname);
         }
 
-
     }
+
+    /**
+     * @return bool
+     */
+    public function isDbRef()
+    {
+        return $this->dbRef;
+    }
+
+    /**
+     * @param bool $dbRef
+     */
+    public function setDbRef($dbRef)
+    {
+        $this->dbRef = $dbRef;
+    }
+
+
+
 
 }
--- app/Models/Users.php
+++ app/Models/Users.php
@@ -27,6 +27,7 @@ class Users extends \Phalcon\Mvc\Model
     public function initialize()
     {
         $this->setSource('users');
+        $this->setDbRef(true);
     }
 
     public function afterSave()
