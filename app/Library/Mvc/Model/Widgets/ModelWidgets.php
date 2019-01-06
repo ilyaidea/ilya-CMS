70e1c9fbab27677a126180cb477bdf672855d901
IlyaIdea <alimansoori71@outlook.com>
2019-01-06 11:16:09 +0330

    Fixed feature widgets

---
+++ app/Library/Mvc/Model/Widgets/ModelWidgets.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Summary File ModelWidgets
+ *
+ * Description File ModelWidgets
+ *
+ * ILYA CMS Created by ILYA-IDEA Company.
+ * @author Ali Mansoori
+ * Date: 1/6/2019
+ * Time: 9:02 AM
+ * @version 1.0.0
+ * @copyright Copyright (c) 2017-2019, ILYA-IDEA Company
+ */
+
+namespace Lib\Mvc\Model\Widgets;
+
+
+class ModelWidgets
+{
+
+}
