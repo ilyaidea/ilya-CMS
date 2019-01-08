9c7ed16764423c520623c3e4c6e996bf451d066c
Faeze <eshaghi_faeze@yahoo.com>
2019-01-08 12:52:24 +0330

    Page Manger: add Keywords

--- app/Modules/System/PageManager/Models/Keywords/ModelKeywords.php
+++ app/Modules/System/PageManager/Models/Keywords/ModelKeywords.php
@@ -8,7 +8,10 @@
 namespace Modules\System\PageManager\Models\Keywords;
 
 use Lib\Events\IModelEvents;
+use Lib\Forms\Form;
+use Lib\Mvc\Controller;
 use Lib\Mvc\Model;
+use Modules\System\PageManager\Models\Pages\ModelPages;
 
 class ModelKeywords extends Model implements IModelEvents
 {
@@ -20,6 +23,26 @@ class ModelKeywords extends Model implements IModelEvents
     public function initialize()
     {
         parent::initialize();
-        $this->setSource('ilya_pages_kewords');
+        $this->setSource('ilya_pages_keywords');
     }
+
+    /**************************************************************************
+     * Static Methods
+     */
+
+    public static function saveKeywords($pageId, $keyword)
+    {
+        $keywords = str_replace(',', '-', $keyword);
+        $keywords = str_replace('_', ' ', $keywords);
+        $keywords = explode('-',$keywords);
+
+        foreach ($keywords as $keyw )
+        {
+            $inst = new self();
+            $inst->setPageId($pageId);
+            $inst->setTitle((trim($keyw)));
+            $inst->save();
+        }
+    }
+
 }
